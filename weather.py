3e26ce042e5a2573032fabf88b644c8f626416d1
likhithalingamaneni99 <likhithalingamaneni99@gmail.com>
2026-06-15 02:24:48 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,21 @@
+import requests
+
+API_KEY = "fe2ce2ca3ceb818ff787f7c8e5045b22"
+
+city = input("Enter City Name: ")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+
+data = response.json()
+
+if response.status_code == 200:
+    print("\nWeather Details")
+    print("----------------")
+    print("City:", data["name"])
+    print("Temperature:", data["main"]["temp"], "°C")
+    print("Humidity:", data["main"]["humidity"], "%")
+    print("Condition:", data["weather"][0]["description"])
+else:
+    print("City not found!")
